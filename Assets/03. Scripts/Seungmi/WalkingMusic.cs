16a21a717580e1ec53c9d86c2f72c440b8a6d798
seungmi <smlee0102@gmail.com>
2023-01-14 12:16:17 +0900

    onplay

--- Assets/03. Scripts/Seungmi/WalkingMusic.cs
+++ Assets/03. Scripts/Seungmi/WalkingMusic.cs
@@ -2,7 +2,6 @@ using System;
 using UnityEngine;
 
 [RequireComponent(typeof(AudioSource))]
-[RequireComponent(typeof(Rigidbody))]
 [RequireComponent(typeof(Collider))]
 
 public class WalkingMusic : MonoBehaviour
@@ -23,7 +22,6 @@ public class WalkingMusic : MonoBehaviour
         if (!other.CompareTag("Player")) return;
 
         _audioSource.Play();
-        Debug.Log("½ÇÇà");
     }
 
     private void OnTriggerExit(Collider other)
